061dff0ada1db7a1052b32fab6c851e5c900d8c6
unknown <roel@D48.nl.desan.net>
2015-01-09 17:25:55 +0100

    glogg version 1.0.2.

--- glogg/tools/chocolateyInstall.ps1
+++ glogg/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ $url = 'http://glogg.bonnefon.org/files/glogg-latest-setup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
