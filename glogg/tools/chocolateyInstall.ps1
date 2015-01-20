2a7cbee2095d2f7db622159a9165c7bfce104b04
unknown <roel@D48.nl.desan.net>
2015-01-20 09:56:23 +0100

    Use dowload URL to explicit version.

--- glogg/tools/chocolateyInstall.ps1
+++ glogg/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'glogg'
 $installerType = 'exe'
-$url = 'http://glogg.bonnefon.org/files/glogg-latest-setup.exe'
+$url = 'http://glogg.bonnefon.org/files/glogg-v1.0.2-1-setup.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
