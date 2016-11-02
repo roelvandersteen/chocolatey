84364b2a7a883c49300df5877b80a78887069ccb
Roel van der Steen <roel@infotron.nl>
2016-11-02 10:57:57 +0100

    Repackaging version 3.14 to 3.14.20161102

--- nettime/nupkg/tools/chocolateyInstall.ps1
+++ nettime/nupkg/tools/chocolateyInstall.ps1
@@ -1,11 +1,19 @@
-﻿$packageName = 'nettime'
-$installerType = 'exe'
-$url = 'http://www.timesynctool.com/NetTimeSetup-314.exe'
-$silentArgs = '/silent'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'nettime'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'http://www.timesynctool.com/NetTimeSetup-314.exe'
+  softwareName  = 'NetTime'
+  checksum      = '107C0E40D46936B166F362090D34AA4C'
+  checksumType  = 'md5'
+  silentArgs   = '/silent'
+  validExitCodes= @(0)
+}
 
 $netTimeSvc = Get-Service NetTimeSvc -ErrorAction SilentlyContinue
 if ($netTimeSvc -and $netTimeSvc.CanStop) { $netTimeSvc.Stop() }
 $netTime = Get-Process NetTime -ErrorAction SilentlyContinue
 if ($netTime) { Stop-Process $netTime.Id }
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
