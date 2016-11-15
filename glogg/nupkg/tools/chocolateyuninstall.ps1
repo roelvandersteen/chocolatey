8aa9ead1e644cd1c59805d1b5e55c418a094a2a0
Roel van der Steen <roel@infotron.nl>
2016-11-15 15:52:13 +0100

    Version 1.1.2
    - New version
    - Added checksums

---
+++ glogg/nupkg/tools/chocolateyuninstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName glogg
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage -PackageName glogg -FileType EXE -SilentArgs "/S" -ValidExitCodes @(0) -File "$file"
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "glogg has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
--- glogg/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'glogg'
-$installerType = 'exe'
-$url = 'http://glogg.bonnefon.org/files/glogg-v1.0.3-setup.exe'
-$url64bit = 'http://glogg.bonnefon.org/files/glogg-v1.1.1-1-x86_64-setup.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64bit" -validExitCodes $validExitCodes
