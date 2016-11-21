e4e9ae5ad8199adb843d7c3593d7804bb41a7331
Roel van der Steen <roel@infotron.nl>
2016-11-21 09:33:28 +0100

    Provide correct 64-bits checksum

--- glogg/nupkg/tools/chocolateyInstall.ps1
+++ glogg/nupkg/tools/chocolateyInstall.ps1
@@ -8,8 +8,9 @@ $packageArgs = @{
   url64bit       = 'http://glogg.bonnefon.org/files/glogg-v1.1.2-x86_64-setup.exe'
   softwareName   = 'glogg'
   checksum       = '0284229FE6476354577EF458901F313C'
-  checksum64     = '0A1D367D3AE8C535E02593C9FC6F8C23'
   checksumType   = 'md5'
+  checksum64     = '0A1D367D3AE8C535E02593C9FC6F8C23'
+  checksumType64 = 'md5'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
