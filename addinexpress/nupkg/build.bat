d13de8b1fc06a1750e2898678c947cf296119ce6
Roel van der Steen <roel@st2x.net>
2018-09-26 18:16:33 +0200

    New package Add-in Express

---
+++ addinexpress/nupkg/build.bat
@@ -0,0 +1,2 @@
+choco pack
+choco push "addinexpress.9.0.4610.nupkg" -ApiKey "admin:zz674999" -Source "https://nuget.st2x.net/nuget/Choco"
---
+++ addinexpress/nupkg/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'
+Get-ChocolateyUnzip "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\Add-in Express.7z" "C:\Program Files (x86)\" -ErrorAction Continue
