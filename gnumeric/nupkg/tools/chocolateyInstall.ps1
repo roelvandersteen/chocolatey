76df5f34cd45c3af0e2507d9aa0208b1587d6b23
Roel van der Steen <roel@st2x.net>
2020-05-31 21:41:33 +0200

    Repackage gnumeric

---
+++ gnumeric/nupkg/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'gnumeric'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url64bit       = 'https://web.archive.org/web/20140702003750if_/https://people.gnome.org/~mortenw/gnumeric/gnumeric-1.12.17-20140610.exe'
+  softwareName   = 'gnumeric'
+  checksum64     = 'A4837A8F818540E9BABA8FC13D2412FA'
+  checksumType64 = 'md5'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ gnumeric/nupkg/tools/chocolateyuninstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName gnumeric
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage -PackageName gnumeric -FileType EXE -SilentArgs "/SD" -ValidExitCodes @(0) -File "$file"
+  }
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "gnumeric has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
