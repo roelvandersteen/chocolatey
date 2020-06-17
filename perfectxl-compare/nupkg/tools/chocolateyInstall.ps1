3d7d7da10d797ed524d52ba3ed4761f4454f1a09
Roel van der Steen <roel@st2x.net>
2020-06-17 15:45:23 +0200

    Version 1.0.3

--- perfectxl-compare/nupkg/tools/chocolateyInstall.ps1
+++ perfectxl-compare/nupkg/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@ $packageArgs = @{
   packageName    = 'perfectxl-compare'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url64bit       = 'https://update.perfectxl.com/perfectxl-compare/win/x64/PerfectXL-Compare-1.0.2.exe'
+  url64bit       = 'https://update.perfectxl.com/perfectxl-compare/win/x64/PerfectXL-Compare-1.0.3.exe'
   softwareName   = 'perfectxl-compare'
-  checksum64     = '6A192DE8427CC291342227E5D7CAD6B8'
-  checksumType64 = 'md5'
+  checksum64     = 'E0997F699BCA8CEDEEA0EA6E93985990CA54B2BE6AC79B35B2F4B3905702EF60'
+  checksumType64 = 'sha256'
   silentArgs     = '/S /allusers'
   validExitCodes = @(0)
 }
