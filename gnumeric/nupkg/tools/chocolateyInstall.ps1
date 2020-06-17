50bf8ec36139ceccddb7bdb30e6495087419c5cc
Roel van der Steen <roel@st2x.net>
2020-06-17 15:37:03 +0200

    Include installer in package.

--- gnumeric/nupkg/tools/chocolateyInstall.ps1
+++ gnumeric/nupkg/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName    = 'gnumeric'
-  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  fileType       = 'exe'
-  url64bit       = 'https://web.archive.org/web/20140702003750if_/https://people.gnome.org/~mortenw/gnumeric/gnumeric-1.12.17-20140610.exe'
-  softwareName   = 'gnumeric'
-  checksum64     = 'A4837A8F818540E9BABA8FC13D2412FA'
-  checksumType64 = 'md5'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  PackageName    = 'gnumeric'
+  UnzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  FileType       = 'exe'
+  File64         = "$(Join-Path $(Split-Path $MyInvocation.MyCommand.Definition) 'gnumeric-1.12.17-20140610.exe')"
+  SoftwareName   = 'gnumeric'
+  Checksum64     = 'A4837A8F818540E9BABA8FC13D2412FA'
+  ChecksumType64 = 'md5'
+  SilentArgs     = '/S'
+  ValidExitCodes = @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
